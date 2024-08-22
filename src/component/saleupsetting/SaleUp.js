c729fd94cd5540c876146443bc99432e63c919b8
learnwithusmansagheer <apnausmansagheer@gmail.com>
2024-08-22 18:19:12 +0500

    feat:fix the saleup portion

--- src/component/saleupsetting/SaleUp.js
+++ src/component/saleupsetting/SaleUp.js
@@ -6,11 +6,11 @@ import './SaleUp.css'
 export default function SaleUp() {
     return (
         <>
-            <div className="saleup  flex justify-evenly  " style={{ borderRadius: '20px', overflow: 'hidden', height: '400px', margin: '40px', background: 'rgb(27, 27, 27)' }}>
-                <div className="laptopImg  w-1/2 h-full overflow-hidden  ">
-                    <img src={Laptop} alt="" style={{ height: '700px', width: '1000px',marginLeft:'px', marginTop: '-170px', zIndex: '0' }} />
+            <div className="saleup  flex   " >
+                <div className="laptopImg    overflow-hidden  ">
+                    <img src={Laptop} alt="" />
                 </div>
-                <div className="saleUpInfo flex flex-col  bg-white justify-start absolute" >
+                <div className="saleUpInfo flex flex-col   justify-start absolute" >
                     <div className="salenewButton flex flex-col">
 
                         <Button
@@ -19,7 +19,7 @@ export default function SaleUp() {
                             className="saleButton w-full   " />
 
                     </div>
-                    <div className="saleupinfo flex flex-col gap-3">
+                    <div className="saleupinfo">
                         <h1 className='sale ' style={{ fontSize: '41px', fontWeight: '700', color: 'rgba(46, 143, 197, 1)' }}>Sale up to 50% off</h1>
                         <p className=' '>12 inch hd display</p>
                     </div>
